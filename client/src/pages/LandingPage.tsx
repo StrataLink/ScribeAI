18ec372cc5aabc3e18dbd998f8c227441e5908b7
Rachel Tsui <rachelt3835@gmail.com>
2023-10-06 22:39:10 -0400

    Landing page edits

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -3,19 +3,27 @@ import "./LandingPage.css";
 import { useNavigate } from "react-router-dom";
 import Navbar from "../components/Navbar";
 
+
 function LandingPage() {
+    const Laptop = require("../images/Laptop.svg") as string;
     const navigate = useNavigate()
   return (
     <div className="landing-container">
       <Navbar/>
       <header>
-        <h1>Welcome to Our Website</h1>
-      </header>
-      <section>
+        <h1>Innovate your note-taking experience</h1>
+
+        <section>
         <p>
-          Hello
+          The go-to platform for taking notes
         </p>
+        <span>
+          Convert speech to meaningful text summaries
+        </span>
       </section>
+      <img style={{width: 500, height: 300}} src={Laptop} alt="imgLaptop" className='imgLaptop'/>
+      <div className="shadow">.</div>
+      </header> 
       <section>
         <button className="cta-button">Get Started</button>
         <button className="register-button" onClick={() => navigate("/register")}>Register</button>
