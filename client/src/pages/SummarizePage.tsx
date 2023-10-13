bf0f55490c7840d2c4c88d4183e4ad6a50bceea1
Rcheng2003 <64754199+Rcheng2003@users.noreply.github.com>
2023-10-13 17:13:49 -0400

    Modified Frontend

--- client/src/pages/SummarizePage.tsx
+++ client/src/pages/SummarizePage.tsx
@@ -1,24 +1,31 @@
 import React, { useState } from 'react';
 import './SummarizePage.css';
+import axios from 'axios'; // Import Axios
 
 function SummarizePage() {
   const [inputText, setInputText] = useState('');
+  const [summary, setSummary] = useState('');
 
   const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setInputText(e.target.value);
   };
+  
 
-  const handleSubmit = () => {
-    // You can handle the text input here, e.g., send it to the server for summarization
-    // For now, we'll just display the input text
-    alert(`Entered Text: ${inputText}`);
+  const handleSubmit = async () => {
+    try {
+      const response = await axios.post('/summarize', { text: inputText });
+      setSummary(response.data.summary);
+    } catch (error) {
+      console.error('Error summarizing text:', error);
+    }
   };
 
   return (
     <div className="SummarizeWhole">
       <h1 className='SummarizeTitle'>Text Summarizer</h1>
       <p className='SummarizeParagraph'>Enter the text you want to summarize:</p>
-      <textarea className='SummarizeText'
+      <textarea
+        className='SummarizeText'
         rows={10}
         cols={50}
         value={inputText}
@@ -27,6 +34,12 @@ function SummarizePage() {
       />
       <br />
       <button className='SummarizeButton' onClick={handleSubmit}>Summarize</button>
+      {summary && (
+        <div className="Summary">
+          <h2>Summary:</h2>
+          <p>{summary}</p>
+        </div>
+      )}
     </div>
   );
 }
