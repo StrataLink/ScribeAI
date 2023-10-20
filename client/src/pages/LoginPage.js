938d8ebd26bb40d58706c5a0c1aa3bf377526f9e
Rachel Tsui <rachelt3835@gmail.com>
2023-10-20 16:32:35 -0400

    Landing Page and LoginPage to js

---
+++ client/src/pages/LoginPage.js
@@ -0,0 +1,48 @@
+// LoginPage.tsx
+import React, { useState } from "react"
+import "./LoginPage.css" // Import your CSS file for styling
+import { useNavigate } from "react-router-dom"
+
+function LoginPage() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleLogin = () => {
+    // Implement your login logic here
+  }
+
+  const navigate = useNavigate()
+
+  return (
+    <div>
+      <div className="login-container">
+        <h2>Login</h2>
+        <form onSubmit={handleLogin}>
+          <div className="form-group">
+            <label>Email:</label>
+            <input
+              type="email"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label>Password:</label>
+            <input
+              type="password"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <button type="submit" onClick={() => navigate("/main")}>
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default LoginPage
