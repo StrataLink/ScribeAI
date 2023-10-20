7becab5dc1f477e39265478ece16efb3650adf00
Rachel Tsui <rachelt3835@gmail.com>
2023-10-20 16:34:50 -0400

    App, RegisterPage, Index to js

---
+++ client/src/pages/RegisterPage.js
@@ -0,0 +1,55 @@
+// RegisterPage.tsx
+import React, { useState } from "react"
+import "./RegisterPage.css" // Import your CSS file for styling
+import { useNavigate } from "react-router-dom"
+
+function RegisterPage() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [confirmPassword, setConfirmPassword] = useState("")
+
+  const handleRegister = () => {
+    // Implement your registration logic here
+  }
+  const navigate = useNavigate()
+
+  return (
+    <div className="register-container">
+      <h2>Register</h2>
+      <form onSubmit={handleRegister}>
+        <div className="form-group">
+          <label>Email:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label>Confirm Password:</label>
+          <input
+            type="password"
+            value={confirmPassword}
+            onChange={e => setConfirmPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit" onClick={() => navigate("/login")}>
+          Register
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default RegisterPage
