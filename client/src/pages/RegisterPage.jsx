6056a0905709e758b1e8615ab5aa1d16bb319e03
Rcheng2003 <64754199+Rcheng2003@users.noreply.github.com>
2023-11-17 07:57:35 -0500

    SignIn and Register css Implemented

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -44,47 +44,51 @@ function RegisterPage() {
   };
 
   return (
+    <div className="register-main">
     <div className="register-container">
       <h2>Register</h2>
       <form onSubmit={handleRegister}>
-        <div className="form-group">
-          <label>Username:</label>
-          <input
-            type="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label>Email:</label>
+        <div className="userR">
+          <i className="fa fa-envelope"></i>         
+        <div className="form-groupR">
           <input
             type="email"
             value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            onChange={e => setEmail(e.target.value)}
             required
+            placeholder="Email"
           />
         </div>
-        <div className="form-group">
-          <label>Password:</label>
+        </div>
+        <div className="userR">
+          <i className="fa fa fa-unlock-alt"></i>
+        <div className="form-groupR">
           <input
             type="password"
             value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={e => setPassword(e.target.value)}
             required
+            placeholder="Password"
           />
         </div>
-        <div className="form-group">
-          <label>Confirm Password:</label>
+        </div>
+        <div className="userR">
+          <i className="fa fa-lock"></i>
+        <div className="form-groupR">
           <input
             type="password"
             value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
+            onChange={e => setConfirmPassword(e.target.value)}
             required
+            placeholder="Confirm Password"
           />
         </div>
-        <button type="submit">Register</button>
+        </div>
+        <button type="submit" onClick={() => navigate("/login")}>
+          Register
+        </button>
       </form>
+    </div>
     </div>
   );
 }
