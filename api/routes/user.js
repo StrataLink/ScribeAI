f705d3c0c95182b3b9741a876c19eb2b6fa3df00
Tashi Sherpa <68435081+TSherpa10@users.noreply.github.com>
2023-11-16 19:04:37 -0500

    sidebar complete, need css

--- api/routes/user.js
+++ api/routes/user.js
@@ -4,82 +4,20 @@ const User = require("../models/User");
 
 const router = express.Router();
 
-// const AWS = require("aws-sdk");
-// AWS.config.update({
-//   region: "us-east-1",
-//   accessKeyId: "AKIAXSI6TO7ANQL2AF6B",
-//   secretAccessKey: process.env.AWS_SK,
-// });
-
-// const s3 = new AWS.S3();
-// const BUCKET_NAME = "sonepfps";
-
-// const multer = require("multer");
-// const multerS3 = require("multer-s3");
-
-// const upload = multer({
-//   storage: multerS3({
-//     s3: s3,
-//     bucket: BUCKET_NAME,
-//     metadata: (req, file, cb) => {
-//       cb(null, { fieldName: file.fieldname });
-//     },
-//     key: (req, file, cb) => {
-//       cb(
-//         null,
-//         `profilePics/${Date.now().toString()}${path.extname(file.originalname)}`
-//       );
-//     },
-//   }),
-//   limits: { fileSize: 1000000 },
-//   fileFilter: (req, file, cb) => {
-//     const fileTypes = /jpeg|jpg|png/;
-//     const extname = fileTypes.test(
-//       path.extname(file.originalname).toLowerCase()
-//     );
-//     const mimetype = fileTypes.test(file.mimetype);
-
-//     if (extname && mimetype) {
-//       return cb(null, true);
-//     } else {
-//       cb("Error: Images only!");
-//     }
-//   },
-// }).single("profilePic");
-
 router.get("/profile", async (req, res) => {
   if (!req.user) {
-    return res.json({ status: "not_logged_in" });
+    return res.status(500).json({ error: "Cannot find a user." });
   }
-
-  const user = await User.findOne({ email: req.user.email });
-  return res.json({ status: "ok", user: user });
+  try {
+    const user = await User.findOne({
+      email: req.user.email,
+    });
+    res.status(200).json({ message: "Found user.", user: user });
+  } catch (e) {
+    res
+      .status(404)
+      .json({ error: "Cannot find a user associated with this email." });
+  }
 });
-
-// router.post("/uploadPfp", (req, res) => {
-//   upload(req, res, async (err) => {
-//     if (err) {
-//       return res.status(500).json({ error: err });
-//     }
-
-//     const fileUrl = req.file.location;
-
-//     try {
-//       const user = await User.findOneAndUpdate(
-//         { email: req.user.email },
-//         { profilePicture: fileUrl },
-//         { new: true }
-//       );
-
-//       if (!user) {
-//         return res.status(404).json({ error: "User not found" });
-//       }
-
-//       res.json({ status: "ok", fileUrl });
-//     } catch (error) {
-//       res.status(500).json({ error });
-//     }
-//   });
-// });
 
 module.exports = router;
--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -1,63 +1,131 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import axios from "axios";
+import { apiURL } from "../utils/URL";
 import "./MainPage.css";
+import Sidebar from "../components/Sidebar";
 
 const MainPage = (props) => {
+  // Use state variables: fill empty with separate components.
+  const navigate = useNavigate();
+  const [live, setLive] = useState(true);
+  const [profileView, setProfileView] = useState();
+  const [colorView, setColorView] = useState();
+  const [subView, setSubView] = useState();
+  const [tooltip, setTooltip] = useState();
+  const [entries, setEntries] = useState([]);
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    // Use state variables: fill empty with separate components.
-    const [live, setLive] = useState(true);
-    const [profileView, setProfileView] = useState();
-    const [colorView, setColorView] = useState();
-    const [subView, setSubView] = useState();
-    const [tooltip, setTooltip] = useState();
-    const [entries, setEntries] = useState([]);
-    
-    const createEntry = () => {
-        // mongo logic - add new entry to collection: entries in DB.
-        // database forEach loop will handle CSS/frontend display - medium
+  const populateUserInfo = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:3001/api/user/profile",
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          withCredentials: true,
+        }
+      );
+      if (response.status != 200) {
+        throw new Error("Not logged in.");
+      } else {
+        setUser(response.data.user);
+        await fetchEntries();
+      }
+    } catch (e) {
+      if (e.message === "Not logged in.") {
+        navigate("/login");
+      } else {
+        console.error("An rror occurred.");
+        navigate("/");
+      }
     }
+  };
 
-    const handleSideBar = () => {
-        // sidebar div will have built in transition (sec), shift absolute position
-        // when click back and forth. starts as extended sidebar - medium
+  const fetchEntries = async () => {
+    try {
+      const response = await axios.get(`${apiURL}/api/entry/get-entries`, {
+        withCredentials: true,
+      });
+      setEntries(response.data.data);
+      setLoading(false);
+    } catch (e) {
+      console.error("Frontend error fetching entries. Error Message:", e);
     }
+  };
 
-    const handleEntry = () => {
-        // change entryView useState variable - easy.
-    }
+  useEffect(() => {
+    populateUserInfo();
+  }, []);
 
-    const handleBookMark = () => {
-        // mongo logic - add boolean to entry so star shows up (important) - easy
-    }
+    const createEntry = async (userRef) => {
+      // mongo logic - add new entry to collection: entries in DB.
+      // database forEach loop will handle CSS/frontend display - medium
+      try {
+        const response = await axios.post(`${apiURL}/api/entry/add-entry`, {
+            user: userRef,
+            title: "Untitled Entry" // todo, make alliteration later?
+        });
+        setEntries(prevEntries => [...prevEntries, response.data.data]); // need to sort in map based on last updated?
+      }
+      catch (e) {
+        console.error("Failed to create a new entry. Error Message:", e);
+      }
+    };
 
-    const handleRecord = () => {
-        // logic - check live boolean, based on that to preprocessing - then display on frontend, main text - hard
-    }
+  /* handleSideBar function is on-hold until further notice, to be done in Sidebar component. */
+  
+  // archaic function decl. Jason's task.
+  const handleEntry = () => {
+    // change entryView useState variable - easy.
+  };
+  // on-hold.
+  const handleBookMark = () => {
+    // mongo logic - add boolean to entry so star shows up (important) - easy
+  };
+  
+  // Jason.
+  const handleRecord = () => {
+    // logic - check live boolean, based on that to preprocessing - then display on frontend, main text - hard
+  };
+  
+  // Jason.
+  const handleLiveRecording = () => {
+    // switch boolean to true if false, easy.
+  };
 
-    const handleLiveRecording = () => {
-        // switch boolean to true if false, easy.
-    }
+  // on-hold.
+  const handleVideoRecording = () => {
+    // switch boolean to false if true, easy.
+  };
+  
+  // on-hold.
+  const handleToolTip = () => {
+    // change tooltip boolean, show tooltip in middle of the screen. (disable tooltip when display div.), easy
+  };
 
-    const handleVideoRecording = () => {
-        // switch boolean to false if true, easy.
-    }
+  // pass to Sidebar, Ryan's task.
+  const handleProfile = (userRef) => {
+    // logic - render profile component (pull from DB based on current user) - medium/hard
+  };
 
-    const handleToolTip = () => {
-        // change tooltip boolean, show tooltip in middle of the screen. (disable tooltip when display div.), easy
-    }
-
-    const handleProfile = () => {
-        // logic - render profile component (pull from DB based on current user) - medium/hard
-    }
-
-    return (
-        <>
-            {/* add sidebar component -> map individual entries with changeable names. */}
-            {/* add entryview component */}
-            {/* add sub, color, profile, and tooltip view here. */}
-        </>
-    )
-}
+  return (
+    <>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <div className="main-wrapper">
+          {/* add sidebar component -> map individual entries with changeable names. */}
+          {console.log(entries)}
+          <Sidebar entries={entries} createFunc={() => createEntry(user)} profileFunc={() => handleProfile(user)}  />
+          {/* add entryview component */}
+          {/* add sub, color, profile, and tooltip view here. */}
+        </div>
+      )}
+    </>
+  );
+};
 
 export default MainPage;
