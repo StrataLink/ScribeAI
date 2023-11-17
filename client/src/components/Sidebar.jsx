ef9c0db440c7a01cf178b886e66220b82b3bc66b
jasn6 <110363877+jasn6@users.noreply.github.com>
2023-11-16 23:01:11 -0500

    temp css styling and formating for main page and sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,24 +1,24 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 import "./Sidebar.css";
 
-const Sidebar = ( {entries, createFunc, profileFunc } ) => {
-
-    const handleCreateEntry = () => {
-        createFunc();
-    }
-
-    return (
-      <div className="sidebar-wrapper">
-        <button onClick={handleCreateEntry}></button>
-        <div className="entrylist-div">{entries.map((entry) => {
-            <div key={entry._id} className="entry-div">
-                <input type="text" />
-            </div>
-        })}</div>
+const Sidebar = ({ entries, createFunc }) => {
+  return (
+    <div className="sidebar">
+      <div className="sidebar-top">
+        <button className="create-button" onClick={createFunc}>
+          +
+        </button>
+        <div className="placeholder-div"></div>
       </div>
-    );
-    
-}
+      <div className="entries">
+        {entries.map((entry) => (
+          <div key={entry._id} className="entry">
+            <span className="entry-title">{entry.title}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Sidebar;
