7fddc038fc1079f405ab3216b31f408f33e1dbb3
jasn6 <110363877+jasn6@users.noreply.github.com>
2023-11-16 22:14:31 -0500

    get single entry endpoint

--- api/routes/entry.js
+++ api/routes/entry.js
@@ -1,38 +1,52 @@
 const express = require("express");
 // const User = require("../models/User.js") might need in the future.
-const Entry = require("../models/Entry.js")
+const Entry = require("../models/Entry.js");
 
 const router = express.Router();
 
-router.post("/add-entry", async(req, res) => {
-    try {
-        const newEntry = await Entry.create({
-            user: req.body.user,
-            title: req.body.title, // todo, make this alliteration.
-            rawDesc: "",
-            summarizedDesc: "",
-        })
-        res.status(200).json({message: "Successfully created new entry.", data: newEntry});
-    }
-    catch (e) {
-        console.error(e);
-        res.status(404).json({message: "Failed creating new entry.", error: e});
-    }
+router.post("/add-entry", async (req, res) => {
+  try {
+    const newEntry = await Entry.create({
+      user: req.body.user,
+      title: req.body.title, // todo, make this alliteration.
+      rawDesc: "",
+      summarizedDesc: "",
+    });
+    res
+      .status(200)
+      .json({ message: "Successfully created new entry.", data: newEntry });
+  } catch (e) {
+    console.error(e);
+    res.status(404).json({ message: "Failed creating new entry.", error: e });
+  }
 });
 
-router.get("/get-entries", async(req, res) => {
-    try {
-        const all = await Entry.find();
-        const response = {
-            message: "Successful got all entries",
-            data: all,
-        }
-        res.status(200).json(response);
+router.get("/get-entries", async (req, res) => {
+  try {
+    const all = await Entry.find();
+    const response = {
+      message: "Successful got all entries",
+      data: all,
+    };
+    res.status(200).json(response);
+  } catch (e) {
+    console.error(e);
+    res.status(404).json({ message: "Unable to retrieve entries" });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    var entry = await Entry.findOne({ _id: req.params.id });
+
+    if (!entry) {
+      return res.status(404).send({ message: "Room not found" });
     }
-    catch (e) {
-        console.error(e)
-        res.status(404).json({message: "Unable to retrieve entries"});
-    }
+
+    return res.status(200).json(entry);
+  } catch (error) {
+    return res.status(500).send({ message: "Server error", error });
+  }
 });
 
 router.delete("/delete-entry/:id", async (req, res) => {
@@ -49,24 +63,22 @@ router.delete("/delete-entry/:id", async (req, res) => {
   }
 });
 
-router.patch("/update-entry/:id", async(req, res) => {
-    try {
-        const updatedEntry = await Entry.findByIdAndUpdate(
-            req.params.id,
-            { $set: req.body },
-            { new: true, runValidators: true },
-        );
-        if (updatedEntry) {
-            res.status(200).json(updatedEntry)
-        }
-        else {
-            res.status(404).json({message: "Entry not found."})
-        }
+router.patch("/update-entry/:id", async (req, res) => {
+  try {
+    const updatedEntry = await Entry.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true, runValidators: true }
+    );
+    if (updatedEntry) {
+      res.status(200).json(updatedEntry);
+    } else {
+      res.status(404).json({ message: "Entry not found." });
     }
-    catch (e) {
-        console.error(e);
-        res.status(500).json({message: "Error updating entry."})
-    }
-})
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ message: "Error updating entry." });
+  }
+});
 
 module.exports = router;
